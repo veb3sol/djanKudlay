8a9ffad74814ea621075a51da4fa067335b712ef
Ruslan <veb3solidity@gmail.com>
2024-01-16 14:48:28 +0200

    less 9

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 # include -- для организации вложености адресов
@@ -24,3 +26,7 @@ urlpatterns = [
     path('', include('news.urls')),    # подключаем адреса приложения
 
 ]
+
+# для отображения картинок на сайте при разработке надо создать путь по которому будем искать медиа файлы
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA__ROOT)
