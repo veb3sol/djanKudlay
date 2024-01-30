f59dbfa2e8644e384557c09b02db70a64c8d55b2
Ruslan <veb3solidity@gmail.com>
2024-01-30 14:20:46 +0200

    less8rep

--- news/views.py
+++ news/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 
 from .models import News
 
-
-
 def index(request):
-    news = News.objects.order_by('-created_at')
-    return render(request, 'news/index.html', context = {'news': news, 'title': 'Список новостей'})
+    print(dir(request)) # посмотреть все методы request
+    return HttpResponse('Hello world')
+
+# def index(request):
+#     news = News.objects.order_by('-created_at')
+#     return render(request, 'news/index.html', context = {'news': news, 'title': 'Список новостей'})
 
 
