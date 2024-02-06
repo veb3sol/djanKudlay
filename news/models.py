4f98be93821984d2ce963121c8ff170203ce4f38
Ruslan <veb3solidity@gmail.com>
2024-02-06 14:05:17 +0200

    less13

--- news/models.py
+++ news/models.py
@@ -16,14 +16,19 @@ class News(models.Model):
     # max_length = 150   -- максимальная длина строки, обьязательный атрибут
     content = models.TextField(blank = True, verbose_name='Контент')
     # blank = True  -- поле не обязательное к заполнению, по умолчанию все поля обьязательны к заполнению
-    created_at = models.DateTimeField(auto_now_add = True, verbose_name='Создано')
-    updated_at = models.DateTimeField(auto_now = True, verbose_name = 'Отредактировано')
+    created_at = models.DateTimeField(auto_now_add = True, verbose_name='Створено')
+    updated_at = models.DateTimeField(auto_now = True, verbose_name = 'Відредактовано')
     # auto_now_add = True  -- запишет только дату создания, при перезаписи данные даты и времени не поменяются
     # auto_now = True  -- перезаписывает дату и время при каждом обновлении
     photo = models.ImageField(upload_to = 'photo/%Y/%m/%d', verbose_name='Фото', blank = True)
     # upload_to = 'photo/%Y/%m/%d'  -- куда сохранять фото при загрузке, Y - год, m - месяц, d - день
-    is_published = models.BooleanField(default = True, verbose_name = 'Опубликовано')
+    is_published = models.BooleanField(default = True, verbose_name = 'Опубліковано')
     # default = True -- значение по умолчанию
+    category = models.ForeignKey('Category', on_delete = models.PROTECT, null = True, verbose_name = 'Категорія')    # связывание с моделью Категории
+    # 'Category' - как строка если класс модели Категории указан ниже
+    # Category - как название класса если класс модели Категории указано выше
+    # on_delete = models.PROTECT  -- не позволяем удалять категории если есть статьи такой категории
+    # null = True  значение может быть null, потому что изначально у нас нету никакой категории
 
     # для того что бы не object представлял обьект а строка - например заголовок
     def __str__(self):
@@ -31,9 +36,19 @@ class News(models.Model):
 
     # как модель будет отображаться в админке
     class Meta:
-        verbose_name = 'Новость' # название в единственном числе
-        verbose_name_plural = 'Новости'   # название в множественном числе
+        verbose_name = 'Новина' # название в единственном числе
+        verbose_name_plural = 'Новини'   # название в множественном числе
         ordering = ['-created_at']  # по какому полю сортировать - и на сайт тоже влияет, можно и по 2 полям
 
+class Category(models.Model):
+    title = models.CharField(max_length = 150, db_index = True, verbose_name = 'Назва категорії')
+    # db_index = True - индексировать для повышения скорости поиска по этому полю
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категорія'
+        verbose_name_plural = 'Категорії'
+        ordering = ['title']
 
