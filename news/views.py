81b2a62feaa9332dfee5dc9c4e165c7cdea3d052
Ruslan <veb3solidity@gmail.com>
2024-02-20 18:39:05 +0200

    less17

--- news/views.py
+++ news/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
+from .models import News, Category
 
 # def index(request):
     # print(dir(request)) # посмотреть все методы request
@@ -9,6 +9,22 @@ from .models import News
 
 def index(request):
     news = News.objects.order_by('-created_at')
-    return render(request, 'news/index.html', context = {'news': news, 'title': 'Список новостей'})
+    categories = Category.objects.all()
+    context = {
+        'news': news,
+        'categories': categories,
+        'title': 'Список новостей'
+    }
+    return render(request, 'news/index.html', context = context)
 
 
+def get_category(request, category_id):
+    news = News.objects.order_by('-created_at').filter(category_id=category_id)
+    categories = Category.objects.all()
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'news': news,
+        'categories': categories,
+        'category': category,
+    }
+    return render(request, 'news/category.html', context = context)
